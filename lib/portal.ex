ce4551117407b3f1bc04e187dc6bcf658affd7aa
Lauren Tan <lauren.elizabeth.tan@gmail.com>
2014-11-25 19:29:12 +1100

    late init

---
+++ lib/portal.ex
@@ -0,0 +1,26 @@
+defmodule Portal do
+  use Application
+
+  # See http://elixir-lang.org/docs/stable/elixir/Application.html
+  # for more information on OTP Applications
+  def start(_type, _args) do
+    import Supervisor.Spec, warn: false
+
+    children = [
+      # Define workers and child supervisors to be supervised
+      # worker(Portal.Worker, [arg1, arg2, arg3])
+    ]
+
+    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Portal.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  defstruct [:left, :right]
+
+  @@doc """
+  Starts transfering `data` from `left` to `right`.
+  """
+
+end
