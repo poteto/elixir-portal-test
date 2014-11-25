9227c6d044649c8871c7a0e29af02517b8318d02
Lauren Tan <lauren.elizabeth.tan@gmail.com>
2014-11-25 19:51:05 +1100

    elixir protocols let us extend behaviour - in this case we extedn the Inspect protocol

--- lib/portal.ex
+++ lib/portal.ex
@@ -41,3 +41,22 @@ defmodule Portal do
   end
 
 end
+
+defimpl Inspect, for: Portal do
+  def inspect(%Portal{left: left, right: right}, _) do
+    left_door  = inspect(left)
+    right_door = inspect(right)
+
+    left_data  = inspect(Enum.reverse(Portal.Door.get(left)))
+    right_data = inspect(Portal.Door.get(right))
+
+    max = max(String.length(left_door), String.length(left_data))
+
+    """
+    #Portal<
+      #{String.rjust(left_door, max)} <=> #{right_door}
+      #{String.rjust(left_data, max)} <=> #{right_data}
+    >
+    """
+  end
+end
